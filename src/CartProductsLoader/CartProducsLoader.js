5506b3b4f403124f711ac52859932dd3e29d2bc3
rashidulub <rashidul016094@gmail.com>
2023-04-05 04:54:07 +0600

    section add

--- src/CartProductsLoader/CartProducsLoader.js
+++ src/CartProductsLoader/CartProducsLoader.js
@@ -1,8 +1,23 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const CartProductsLoader= async()=>{
     const loadProducts =await fetch('products.json');
     const products = await loadProducts.json();
-    console.log(products);
-    return products;
+    // console.log(products);
+    const storeCart = getShoppingCart();
+    const saveCart =[];
+   
+
+    for(const id in storeCart){
+    const addedProduct = products.find(pd=>pd.id ===id)
+    if(addedProduct){
+        const quantity = storeCart[id];
+        addedProduct.quantity=quantity;
+        saveCart.push(addedProduct);
+    }
+
+    }
+    return saveCart;
 }
 
 
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import Review from '../Review/Review';
 
 const Orders = () => {
-    const products= useLoaderData();
-    console.log(products);
+    const cart= useLoaderData();
+   
     return (
         <div className="shop-container">
-            <div>
-            <h1>order page: {products.length} :</h1>
+            <div className='review-container'>
+            {
+                cart.map(product=><Review
+                key={product.id}
+                product={product}
+                ></Review>)
+            }
             </div>
         <div className="cart-container">
-        <Cart cart={([])}></Cart>
+        <Cart cart={cart}></Cart>
         </div>
         
 
